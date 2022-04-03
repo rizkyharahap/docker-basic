8909159a973fb2f3ae3ad7d84f1d88d471537fde
rizkyharahap <rizkyhh1999@gmail.com>
2022-04-03 22:03:48 +0700

    Learning Docker Image

---
+++ README.md
@@ -0,0 +1,5 @@
+Learning Docker from Programmer Zaman Now
+
+link : https://www.youtube.com/watch?v=3_yxVjV88Zk&list=PL-CtdCApEFH9P46mnb4yJlQgX_IyQ0uF7
+
+
---
+++ docker-image.sh
@@ -0,0 +1,10 @@
+# For see all list installed docker images
+docker image ls
+
+# For install docker image with tag latest
+# You can change after : to any tag
+docker image pull redis:latest
+
+# For remove docker image
+docker image rm redis:latest
+
